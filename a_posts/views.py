cb379e76b862edeb98c0cf78a6bf6d38b3201e45
Ricardo Viana <richard@Ricardos-MacBook-Pro.local>
2024-06-29 10:18:35 -0300

    create pages

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def home_view(request):
-    title = "Wlcome to Django"
-    return render(request, 'index.html', {'title': title})
+    return render(request, 'a_posts/home.html')
